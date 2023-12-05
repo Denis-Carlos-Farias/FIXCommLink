fbaded943e71b0543cca15be71eea95902fe44c5
Denis Carlos de Farias <denis_carlos_farias@hotmail.com>
2023-12-04 23:52:25 -0300

    Add project files.

---
+++ FIXCommLink.Initiator/Program.cs
@@ -0,0 +1,26 @@
+﻿using FIXCommLink.Service;
+using QuickFix;
+using QuickFix.Transport;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        SessionSettings settings = new SessionSettings("YourInitiatorConfigFile.cfg");
+        IMessageStoreFactory storeFactory = new FileStoreFactory(settings);
+        ILogFactory logFactory = new FileLogFactory(settings);
+        IMessageFactory messageFactory = new DefaultMessageFactory();
+
+        SocketInitiator initiator = new SocketInitiator(
+            new FixAcceptorService(),
+            storeFactory,
+            settings,
+            logFactory,
+            messageFactory
+        );
+
+        initiator.Start();
+        Console.ReadKey();
+        initiator.Stop();
+    }
+}
---
+++ FIXCommLink.Service/FixAcceptorService.cs
@@ -0,0 +1,29 @@
+﻿using QuickFix;
+
+namespace FIXCommLink.Service;
+
+public class FixAcceptorService : MessageCracker, IApplication
+{
+    public void OnMessage(QuickFix.FIX44.NewOrderSingle message, SessionID sessionID) { }
+
+    public void OnMessage(QuickFix.FIX44.ExecutionReport message, SessionID sessionID) { }
+
+    public void OnCreate(SessionID sessionID)
+    {
+        Console.WriteLine("Sessão criada: " + sessionID.ToString());
+    }
+
+    public void OnLogon(SessionID sessionID)
+    {
+        Console.WriteLine("Logon recebido para a sessão: " + sessionID.ToString());
+    }
+
+    public void OnLogout(SessionID sessionID)
+    {
+        Console.WriteLine("Logout recebido para a sessão: " + sessionID.ToString());
+    }
+    public void FromAdmin(Message message, SessionID sessionID) { }
+    public void ToAdmin(Message message, SessionID sessionID) { }
+    public void FromApp(Message message, SessionID sessionID) { }
+    public void ToApp(Message message, SessionID sessionID) { }
+}
