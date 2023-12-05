bb92b8279da1a7ac153f5916cdd64dbc2f771318
Denis Carlos de Farias <denis_carlos_farias@hotmail.com>
2023-12-04 23:56:30 -0300

    Implementado Initiator class

--- FIXCommLink.Initiator/Program.cs
+++ FIXCommLink.Initiator/Program.cs
@@ -6,21 +6,48 @@ class Program
 {
     static void Main(string[] args)
     {
-        SessionSettings settings = new SessionSettings("YourInitiatorConfigFile.cfg");
-        IMessageStoreFactory storeFactory = new FileStoreFactory(settings);
-        ILogFactory logFactory = new FileLogFactory(settings);
-        IMessageFactory messageFactory = new DefaultMessageFactory();
+        Console.WriteLine("=============");
+        Console.WriteLine("This is only an example program.");
+        Console.WriteLine("It's a simple server (e.g. Acceptor) app that will let clients (e.g. Initiators)");
+        Console.WriteLine("connect to it.  It will accept and display any application-level messages that it receives.");
+        Console.WriteLine("Port is 5001.");
+        Console.WriteLine("(see Session.cfg for configuration details)");
+        Console.WriteLine("=============");
 
-        SocketInitiator initiator = new SocketInitiator(
-            new FixAcceptorService(),
-            storeFactory,
-            settings,
-            logFactory,
-            messageFactory
-        );
+        if (args.Length != 1)
+        {
+            Console.WriteLine("usage: SimpleAcceptor CONFIG_FILENAME");
+            System.Environment.Exit(2);
+        }
 
-        initiator.Start();
-        Console.ReadKey();
-        initiator.Stop();
+        try
+        {
+            SessionSettings settings = new SessionSettings(args[0]);
+            IMessageStoreFactory storeFactory = new FileStoreFactory(settings);
+            ILogFactory logFactory = new FileLogFactory(settings);
+            IMessageFactory messageFactory = new DefaultMessageFactory();
+
+            SocketInitiator initiator = new SocketInitiator(
+                new FixAcceptorService(),
+                storeFactory,
+                settings,
+                logFactory,
+                messageFactory
+            );
+
+            initiator.Start();
+            Console.ReadKey();
+            initiator.Stop();
+        }
+        catch (System.Exception e)
+        {
+            Console.WriteLine("==FATAL ERROR==");
+            Console.WriteLine(e.ToString());
+        }
     }
+
+
+
+
+
 }
